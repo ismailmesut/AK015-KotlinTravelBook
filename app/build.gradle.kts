4c253ce0d56b115fa5138cc8217bc27b94ffd46e
ismailmesut <ismailmesutmujde@gmail.com>
2024-09-05 17:23:48 +0300

    updated: ui design

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -46,6 +46,7 @@ dependencies {
     implementation(libs.material)
     implementation(libs.play.services.maps)
     implementation(libs.androidx.constraintlayout)
+    implementation(libs.androidx.activity)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
